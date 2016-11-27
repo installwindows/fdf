e824d7d3385078db48c5186156798e03e79791c0
Vincent Arnaud <varnaud@student.42.us.org>
2016-11-26 23:12:01 -0800

    kinda working

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: varnaud <varnaud@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/29 15:23:27 by varnaud           #+#    #+#             */
-/*   Updated: 2016/11/26 00:38:08 by varnaud          ###   ########.fr       */
+/*   Updated: 2016/11/26 20:51:04 by varnaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -33,8 +33,8 @@ void	draw_line(void *mlx_ptr, void *mlx_window, t_pos a, t_pos b, int color)
 		steps = ft_abs(dx);
 	else
 		steps = ft_abs(dy);
-	Xincrement = dx / (float) steps;
-	Yincrement = dy / (float) steps;
+	Xincrement = (float) dx / (float) steps;
+	Yincrement = (float) dy / (float) steps;
 	while (steps--)
 	{
 		a.x += Xincrement;
@@ -48,7 +48,7 @@ t_grid	*quick_and_dirty(char *file)
 	char	*line;
 	t_grid	*grid;
 
-	grid = malloc(sizeof(grid));
+	grid = malloc(sizeof(t_grid));
 	grid->x = 0;
 	grid->y = 0;
 	if ((fd = open(file, O_RDONLY)) == -1)
@@ -61,6 +61,11 @@ t_grid	*quick_and_dirty(char *file)
 		free(line);
 	}
 	grid->dots = malloc(sizeof(int*) * grid->y);
+	if (grid->dots == NULL)
+	{
+		printf("grid->dots is NULL\n");
+		return (NULL);
+	}
 	close(fd);
 	return (grid);
 }
@@ -74,11 +79,18 @@ void	set_pos(t_grid *grid)
 	i = 0;
 	while (i < grid->y)
 	{
-		grid->pos[i] = malloc(sizeof(t_pos) * grid->y);
+		grid->pos[i] = malloc(sizeof(t_pos) * grid->x);
 		j = 0;
 		while (j < grid->x)
 		{
-			grid->pos[i][j].x = PADDING + INCREMENT * i
+			grid->pos[i][j].x = (float)INCREMENT * j;
+			grid->pos[i][j].y = (float)INCREMENT * i;
+			int x = grid->pos[i][j].x;
+			int y = grid->pos[i][j].y;
+			float angle = 30*M_PI/180;
+
+			grid->pos[i][j].x = PADDING + (x * cos(angle) - y * sin(angle));
+			grid->pos[i][j].y = PADDING + (y * cos(angle) + x * sin(angle)) - grid->dots[i][j] * 1.5;
 			j++;
 		}
 		i++;
@@ -128,20 +140,31 @@ t_grid	*read_file(char *file)
 
 	i = 0;
 	grid = quick_and_dirty(file);
+	if (grid == NULL)
+	{
+		printf("quick_and_dirty failed\n");
+		return (NULL);
+	}
 	printf("after quick %d %d\n", grid->x, grid->y);
 	fd = open(file, O_RDONLY);
 	while (get_next_line(fd, &line))
 	{
 		printf("line: |%s|\n", line);
 		grid->dots[i] = malloc(sizeof(int) * grid->x);
+		if (grid->dots[i] == NULL)
+		{
+			printf("grid->dots[%d] is NULL\n", i);
+			return (NULL);
+		}
 		split = ft_strsplit(line, ' ');
 		j = 0;
 		while (split[j])
 		{
 			grid->dots[i][j] = ft_atoi(split[j]);
-			free(split[j++]);
+			//free(split[j]);
+			j++;
 		}
-		free(split);
+		//free(split);
 		free(line);
 		i++;
 	}
@@ -150,11 +173,33 @@ t_grid	*read_file(char *file)
 	return (grid);
 }
 
-
-
-void	display_grid(t_grid *grid)
+void	display_grid(t_grid *grid, void *mlx_ptr, void *mlx_window)
 {
+	int		i;
+	int		j;
+	t_pos	b;
 
+	i = 0;
+	while (i < grid->y)
+	{
+		j = 0;
+		while (j < grid->x)
+		{
+			printf("(%d, %d) ", i, j);
+			if (j + 1 < grid->x)
+				b = grid->pos[i][j + 1];
+			else
+				b = grid->pos[i][j];
+			draw_line(mlx_ptr, mlx_window, grid->pos[i][j], b, 0xFFFFFF);
+			if (i + 1 < grid->y)
+				b = grid->pos[i + 1][j];
+			else
+				b = grid->pos[i][j];
+			draw_line(mlx_ptr, mlx_window, grid->pos[i][j], b, 0xFFFFFF);
+			j++;
+		}
+		i++;
+	}
 }
 
 void	testing(void)
@@ -174,9 +219,14 @@ void	testing(void)
 	draw_line(mlx_ptr, mlx_window, a, b, 0xFF0000);
 	a.x = 80;
 	a.y = 80;
-	b.x = 169;
-	b.y = 571;
+	b.x = 160;
+	b.y = 100;
 	draw_line(mlx_ptr, mlx_window, a, b, 0x00FF00);
+	a.x = 200;
+	a.y = 200;
+	b.x = 200;
+	b.y = 200;
+	draw_line(mlx_ptr, mlx_window, a, b, 0x0000FF);
 	mlx_loop(mlx_ptr);
 }
 
@@ -187,6 +237,11 @@ int		main(int argc, char **argv)
 	if (argc == 2)
 	{
 		t_grid *grid = read_file(*++argv);
+		if (grid == NULL)
+		{
+			printf("main grid is NULL\n");
+			return (0);
+		}
 		int	i;
 		int	j;
 		for (i = 0; i < grid->y; i++)
@@ -195,6 +250,19 @@ int		main(int argc, char **argv)
 				printf("%d ", grid->dots[i][j]);
 			printf("\n");
 		}
+		set_pos(grid);
+		for (i = 0; i < grid->y; i++)
+		{
+			printf("%d:\n", i);
+			for (j = 0; j < grid->x; j++)
+				printf("j:%d(%.0f,%.0f)  ", j, grid->pos[i][j].x, grid->pos[i][j].y);
+			printf("\n\n");
+		}
+		printf("ALLL DONDEEE\n");
+		void *mlx_ptr = mlx_init();
+		void *mlx_window = mlx_new_window(mlx_ptr, WIN_HEIGHT, WIN_WIDTH, "Ayy");
+		display_grid(grid, mlx_ptr, mlx_window);
+		mlx_loop(mlx_ptr);
 	}
 	else
 	{
--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: varnaud <varnaud@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/22 22:11:15 by varnaud           #+#    #+#             */
-/*   Updated: 2016/11/24 01:12:34 by varnaud          ###   ########.fr       */
+/*   Updated: 2016/11/26 20:35:47 by varnaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 # define FDF_H
 # define WIN_HEIGHT 750
 # define WIN_WIDTH 750
+# define PADDING 250
+# define INCREMENT 20
 
 typedef struct	s_pos
 {
-	int			x;
-	int			y;
+	float		x;
+	float		y;
 	int			z;
 }				t_pos;
 
